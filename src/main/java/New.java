16a989ffe7db7c3de84467e212d48cfa246acc95
Eg-Krutalevich <egor1.krutalevich@mail.ru>
2025-09-17 15:29:35 +0300

    fix: new commit

---
+++ src/main/java/New.java
@@ -0,0 +1,38 @@
+import lombok.*;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+class Person {
+    String name;
+    int age;
+
+}
+
+public class New {
+
+    public static List<String> func(List<Person> personList) {
+        return personList.stream()
+                .filter(str -> !str.getName().endsWith("w"))
+                .sorted(Comparator.comparingInt(Person::getAge).reversed())
+                .map(x -> x.getName().toUpperCase())
+                .distinct()
+                .toList();
+    }
+
+    public static void main(String[] args) {
+        List<Person> persons = new ArrayList<>();
+        persons.add(new Person("new", 9));
+        persons.add(new Person("new6", 89));
+        persons.add(new Person("new89", 13));
+        persons.add(new Person("new5", 81));
+
+        System.out.println(func(persons));
+    }
+}
